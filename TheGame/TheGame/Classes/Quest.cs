4679fd6c76a9250c88c61a9a824fc36d1cf2cba6
SKarastoyanov <skarastoqnov@gmail.com>
2014-02-21 17:03:46 +0200

    DropGift

--- TheGame/TheGame/Classes/Quest.cs
+++ TheGame/TheGame/Classes/Quest.cs
@@ -1,13 +1,23 @@
 ﻿namespace TheGame
 {
     using System;
+    using System.Collections.Generic;
+    using TheGame.Classes;
 
     public class Quest : Action
     {
+        private List<GameObject> gifts;
+
         public Quest(string name, string description, ActionType actionType, int timeCost, int energyCost, bool isCompleted)
             : base(name, description, actionType, timeCost, energyCost, isCompleted)
         {
 
+        }
+
+        //methods
+        public void DropGift()
+        {
+            //to implement later
         }
 
         public override void CompleteAction()
