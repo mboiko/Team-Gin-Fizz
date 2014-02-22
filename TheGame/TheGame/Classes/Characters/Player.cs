abef41b4084a14c3c0ae01e2c20c3eefb46ab499
Producenta <producenta@gmail.com>
2014-02-22 12:06:36 +0200

    Player

---
+++ TheGame/TheGame/Classes/Characters/Player.cs
@@ -0,0 +1,21 @@
+﻿namespace TheGame.Classes
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Player : Character
+    {
+        private int level;
+        private int score;
+        private List<Skill> skills;
+        //private List<Mission> currentMissions;
+        private PlayerType playerType;
+        private char gender;
+        //private Set<Item> inventory;
+        //private Set<Item> equippedItems;
+        private int equippedItemsSize;
+        private double efficiency;
+        //private Location location;
+
+    }
+}
