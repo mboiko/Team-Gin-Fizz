b97b4806237990aaca928aaf1c7fb43ed727d5e7
TarliT <tarlitan@gmail.com>
2014-02-22 12:11:22 +0200

    GameObject properties

--- TheGame/TheGame/Classes/Items/GameObject.cs
+++ TheGame/TheGame/Classes/Items/GameObject.cs
@@ -12,6 +12,48 @@
         private string name;
         private string description;
 
+        public string Name 
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Name cannot be null or empty.");
+                }
+
+                if (value.Length < 2)
+                {
+                    throw new ArgumentOutOfRangeException("Name cannot be less than 2 symbols");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public string Description 
+        {
+            get
+            {
+                return this.description;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Description cannot be null or empty.");
+                }
+
+                if (value.Length < 2)
+                {
+                    throw new ArgumentOutOfRangeException("Description cannot be less than 2 symbols");
+                }
+            }
+        }
+
         #endregion
 
         #region Methods
