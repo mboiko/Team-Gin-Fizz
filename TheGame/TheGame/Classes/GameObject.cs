398db8d3d7e9ab0f5f7601add0d3e45b50da3db4
emilVenkov12 <emil.venkov12@gmail.com>
2014-02-24 11:09:45 +0200

    GameObject added

---
+++ TheGame/TheGame/Classes/GameObject.cs
@@ -0,0 +1,54 @@
+﻿namespace TheGame.Classes
+{
+    using System;
+
+    public abstract class GameObject
+    {
+        #region Fields and Properties
+
+        private string name;
+
+        public string Name 
+        {
+            get
+            {
+                return this.name;
+            }
+            protected set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Name cannot be null or empty.");
+                }
+
+                if (value.Length < 2)
+                {
+                    throw new ArgumentOutOfRangeException("Name cannot be less than 2 symbols.");
+                }
+
+                this.name = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public GameObject(string name)
+        {
+            this.Name = name;
+        }
+
+        #endregion
+
+        #region Methods
+
+        //TODO: ovverid to ..ToString
+        public override string ToString()
+        {
+            return string.Format("{0}", this.name);
+        }
+
+        #endregion
+    }
+}
