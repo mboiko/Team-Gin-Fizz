283c863937eda693917d231fb7f9d966d3d1c5a5
SKarastoyanov <skarastoqnov@gmail.com>
2014-02-26 15:32:35 +0200

    Lot of changes

---
+++ TheGame/TheGame/Building.cs
@@ -0,0 +1,36 @@
+﻿namespace TheGame
+{
+    using System;
+    using System.Collections.Generic;
+
+    using TheGame.Classes.Characters;
+    using TheGame.Classes;
+
+    public class Building
+    {
+        private string name;
+        private Point2D location;
+        private Character person;
+
+        public Building(string initialName, Point2D initialLocation, Character initialPerson)
+        {
+            this.Name = initialName;
+            this.location = initialLocation;
+            this.person = initialPerson;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Bilding name cannot be null or empty!");
+                }
+
+                this.name = value;
+            }
+        }
+    }
+}
--- TheGame/TheGame/Classes/Items/Equipment.cs
+++ TheGame/TheGame/Classes/Items/Equipment.cs
@@ -2,6 +2,9 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Text;
+    using System.Windows.Forms;
+
     using TheGame.Classes.Characters;
 
     public class Equipment : Item, IWearable
@@ -14,12 +17,24 @@
         }
 
         //method
-        public void IncreaseBaseSkill(Hero hero)
+        public void Compare(Item initialItem)
         {
-            for (int skill = 0; skill < hero.Skills.Count; skill++)
+            var result = new StringBuilder();
+            result.AppendLine(string.Format("{0} : {1}", this.Name.PadLeft(32), initialItem.Name.PadRight(32)));
+            result.AppendLine();
+
+            for (int skill = 0; skill < this.BonusSkills.Count; skill++)
             {
-                hero.Skills[skill].BaseSkillValue += BonusSkills[skill].BaseSkillValue;
+                string skillName = this.BonusSkills[skill].Name; 
+                int firstItemValue = this.BonusSkills[skill].BaseSkillValue;
+                int secondItemValue = initialItem.BonusSkills[skill].BaseSkillValue;
+
+                result.AppendLine(string.Format("{0} {1} : {2}",
+                    skillName.PadLeft(31 - firstItemValue.ToString().Length - skillName.Length),
+                    firstItemValue, secondItemValue.ToString().PadRight(32)));
             }
+
+            MessageBox.Show(result.ToString(), "Equipment Comparison", MessageBoxButtons.OK);
         }
 
         public override string ToString()
